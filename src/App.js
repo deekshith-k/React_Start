205b64499a828a6848bace7f84eb922b81150507
deekshith <deekshithkumar850@gmail.com>
2023-11-23 06:01:53 +0530

    Redux concept

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Products from './Component/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+    <h2>E-Commerce Redux Product</h2><hr/>
+    <div className="row">
+      <div class="col-md-8">
+        <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart}/>
+      </div>
+    </div>
     </div>
   );
 }
